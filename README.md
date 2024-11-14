62f942e606b81e6bc92583d7127a88ab1f194e1d
muhammadzakyalfarizi060605 <muhammadzakyalfarizi060605@gmail.com>
2024-11-14 12:48:43 +0700

    W09: Jawaban soal 3

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Saya telah berhasil mendeply proyek saya menggunakan server Vercel dan menginteg
 ## Soal 2
 ## Jelaskan apa yang telah anda pelajari ?
 Saya telah berhasil membuat database PostgreSQL di dalam server Vercel dan menghubungkannya dengan proyek saya, sehingga aplikasi dapat berinteraksi langsung dengan database tersebut untuk menyimpan dan mengelola data secara efisien.
+
+## Soal 3
+## Jelaskan apa yang telah anda pelajari ?
+Saya berhasil melakukan proses seed untuk menambahkan data awal, yaitu mengirim data dari file data.js ke dalam database PostgreSQL di server Vercel.
--- src/seeder/seed.js
+++ src/seeder/seed.js
@@ -1,11 +1,6 @@
-const { db } = require('@vercel/postgres');
-const {
-  invoices,
-  customers,
-  revenue,
-  users,
-} = require('data.js');
-const bcrypt = require('bcrypt');
+const { db } = require("@vercel/postgres");
+const { invoices, customers, revenue, users } = require("./data.js");
+const bcrypt = require("bcrypt");
 
 async function seedUsers(client) {
   try {
@@ -31,7 +26,7 @@ async function seedUsers(client) {
         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
         ON CONFLICT (id) DO NOTHING;
       `;
-      }),
+      })
     );
 
     console.log(`Seeded ${insertedUsers.length} users`);
@@ -41,7 +36,7 @@ async function seedUsers(client) {
       users: insertedUsers,
     };
   } catch (error) {
-    console.error('Error seeding users:', error);
+    console.error("Error seeding users:", error);
     throw error;
   }
 }
@@ -70,8 +65,8 @@ async function seedInvoices(client) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
         ON CONFLICT (id) DO NOTHING;
-      `,
-      ),
+      `
+      )
     );
 
     console.log(`Seeded ${insertedInvoices.length} invoices`);
@@ -81,7 +76,7 @@ async function seedInvoices(client) {
       invoices: insertedInvoices,
     };
   } catch (error) {
-    console.error('Error seeding invoices:', error);
+    console.error("Error seeding invoices:", error);
     throw error;
   }
 }
@@ -109,8 +104,8 @@ async function seedCustomers(client) {
         INSERT INTO customers (id, name, email, image_url)
         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
         ON CONFLICT (id) DO NOTHING;
-      `,
-      ),
+      `
+      )
     );
 
     console.log(`Seeded ${insertedCustomers.length} customers`);
@@ -120,7 +115,7 @@ async function seedCustomers(client) {
       customers: insertedCustomers,
     };
   } catch (error) {
-    console.error('Error seeding customers:', error);
+    console.error("Error seeding customers:", error);
     throw error;
   }
 }
@@ -144,8 +139,8 @@ async function seedRevenue(client) {
         INSERT INTO revenue (month, revenue)
         VALUES (${rev.month}, ${rev.revenue})
         ON CONFLICT (month) DO NOTHING;
-      `,
-      ),
+      `
+      )
     );
 
     console.log(`Seeded ${insertedRevenue.length} revenue`);
@@ -155,7 +150,7 @@ async function seedRevenue(client) {
       revenue: insertedRevenue,
     };
   } catch (error) {
-    console.error('Error seeding revenue:', error);
+    console.error("Error seeding revenue:", error);
     throw error;
   }
 }
@@ -173,7 +168,7 @@ async function main() {
 
 main().catch((err) => {
   console.error(
-    'An error occurred while attempting to seed the database:',
-    err,
+    "An error occurred while attempting to seed the database:",
+    err
   );
 });
